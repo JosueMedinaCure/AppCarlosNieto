c62ec030135828d1f0b6132a5bc7980b440640b8
JosueMedinaCure <jmcure2@gmail.com>
2024-10-23 16:26:06 -0500

    coment

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let currentSlide = 0;
 const carousel = document.querySelector('.imagenes');
 const totalSlides = document.querySelectorAll('.imagenes .card').length;
 
+
+
 // Mover el carrusel en la dirección dada (1 adelante, -1 atrás)
 function moveSlide(direction) {
     currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
     const offset = -currentSlide * 35; // Ajuste basado en el 35% del tamaño de las imágenes
 carousel.style.transform = `translateX(${offset}%)`;
+
 
 }
 
@@ -22,7 +25,7 @@ let autoSlideInterval = setInterval(autoSlide, 6000); // Cambia cada 3 segundos
 function restartAutoSlide(direction) {
     clearInterval(autoSlideInterval); // Detener el desplazamiento automático
     moveSlide(direction);             // Mover el carrusel manualmente
-    autoSlideInterval = setInterval(autoSlide, 8000); // Reiniciar el desplazamiento automático
+    autoSlideInterval = setInterval(autoSlide, 1000); // Reiniciar el desplazamiento automático
 }
 
 // Eventos de clic en los botones de navegación
@@ -33,3 +36,19 @@ document.getElementById('prevBtn').addEventListener('click', () => {
 document.getElementById('nextBtn').addEventListener('click', () => {
     restartAutoSlide(1); // Mover hacia adelante
 });
+
+
+
+// Detectar si el dispositivo es móvil
+function isMobileDevice() {
+    window.innerWidth <= 768;
+    
+    if (window.innerWidth < 768) {
+        clearInterval(autoSlideInterval); // Detiene el carrusel
+    
+    } else { // Detiene el carrusel
+        startCarousel()    
+    }// Puedes ajustar el ancho de acuerdo a lo que consideres un "dispositivo móvil"
+}
+
+isMobileDevice()
